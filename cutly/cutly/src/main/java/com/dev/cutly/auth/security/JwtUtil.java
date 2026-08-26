90607b97b2b27803ab48668084c70f8f668982da
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-08-26 20:59:20 -0300

    generarToken

--- cutly/cutly/src/main/java/com/dev/cutly/auth/security/JwtUtil.java
+++ cutly/cutly/src/main/java/com/dev/cutly/auth/security/JwtUtil.java
@@ -1,5 +1,7 @@
 package com.dev.cutly.auth.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -7,6 +9,7 @@ import io.jsonwebtoken.security.Keys;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Date;
 
 @Component
 public class JwtUtil {
@@ -16,5 +19,14 @@ public class JwtUtil {
 
     public JwtUtil(@Value("${security.jwt.secret}") String secretKey) {
         this.singingKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generarToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRACION))
+                .signWith(singingKey, SignatureAlgorithm.HS256)
+                .compact();
     }
 }
