b6b639fc84319cb976c625a15a6743837b7d990e
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-08-26 20:59:24 -0300

    Update AuthService.java

--- cutly/cutly/src/main/java/com/dev/cutly/auth/service/AuthService.java
+++ cutly/cutly/src/main/java/com/dev/cutly/auth/service/AuthService.java
@@ -4,6 +4,7 @@ import com.dev.cutly.auth.dto.AuthResponse;
 import com.dev.cutly.auth.dto.RegistroRequest;
 import com.dev.cutly.auth.model.Usuario;
 import com.dev.cutly.auth.repository.UsuarioRepository;
+import com.dev.cutly.auth.security.JwtUtil;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +13,12 @@ public class AuthService {
 
     private final UsuarioRepository usuarioRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
-    public AuthService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
+    public AuthService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
         this.usuarioRepository = usuarioRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
 
@@ -32,7 +35,7 @@ public class AuthService {
         usuario.setContrasena(passwordEncoder.encode(request.contrasena()));
 
         Usuario usuarioGuardado = usuarioRepository.save(usuario);
-        String token = "token";
+        String token = jwtUtil.generarToken(usuarioGuardado.getEmail());
 
         return new AuthResponse(token, usuarioGuardado.getUsuarioId(),
                 usuarioGuardado.getNombreUsuario(), usuarioGuardado.getNombre(),
