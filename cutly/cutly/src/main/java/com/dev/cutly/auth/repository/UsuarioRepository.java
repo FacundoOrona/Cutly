b02c8b1a5bf2be4a1f28dbc7e0128f3bc2a53b1e
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-08-26 20:40:54 -0300

    Create UsuarioRepository.java

---
+++ cutly/cutly/src/main/java/com/dev/cutly/auth/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.dev.cutly.auth.repository;
+
+import com.dev.cutly.auth.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+}
