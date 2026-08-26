d5baa6cb843f1bf89cb63871fea42d1abd1d40a6
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-08-26 20:14:34 -0300

    Create Usuario.java

---
+++ cutly/cutly/src/main/java/com/dev/cutly/auth/model/Usuario.java
@@ -0,0 +1,47 @@
+package com.dev.cutly.auth.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "usuario_id")
+    private Long usuarioId;
+
+    @NotBlank(message = "El nombre de usuario no debe estar vacio")
+    @Column(nullable = false)
+    @Size(max = 20, min = 5, message = "El nombre de usuario debe contener entre 5 y 20 caracteres")
+    private String nombreUsuario;
+
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @Size(min = 2, max = 20, message = "El nombre debe tener entre 2 y 20 caracteres")
+    @Column(nullable = false, length = 20)
+    private String nombre;
+
+    @NotBlank(message = "El apellido no puede estar vacío")
+    @Size(min = 2, max = 20, message = "El apellido debe tener entre 2 y 20 caracteres")
+    @Column(nullable = false, length = 20)
+    private String apellido;
+
+    @NotBlank(message = "El email no puede estar vacio")
+    @Email(message = "El formato del email no es valido")
+    @Column(nullable = false)
+    private String email;
+
+    @NotBlank(message = "La contraseña no puede estar vacia")
+    @Column(name = "password_hash", nullable = false, length = 50)
+    @Size(min = 8, max = 50)
+    private String contrasena;
+}
