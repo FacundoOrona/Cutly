1e96c0872306778b37937fd2b7226f69a0867fd8
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-08-26 19:43:08 -0300

    creacion del proyecto api rest cutly

---
+++ cutly/cutly/src/main/java/com/dev/cutly/CutlyApplication.java
@@ -0,0 +1,13 @@
+package com.dev.cutly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CutlyApplication.class, args);
+	}
+
+}
---
+++ cutly/cutly/src/test/java/com/dev/cutly/CutlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.cutly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CutlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
