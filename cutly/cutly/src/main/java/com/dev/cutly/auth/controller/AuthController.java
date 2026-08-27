3eb507c5619947da7082a426dfb4dc23e0bb4fa0
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-08-27 16:58:28 -0300

    endpoint de registro

--- cutly/cutly/src/main/java/com/dev/cutly/auth/controller/AuthController.java
+++ cutly/cutly/src/main/java/com/dev/cutly/auth/controller/AuthController.java
@@ -1,9 +1,33 @@
 package com.dev.cutly.auth.controller;
 
+import com.dev.cutly.auth.dto.AuthResponse;
+import com.dev.cutly.auth.dto.RegistroRequest;
+import com.dev.cutly.auth.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController (AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/registro")
+    public ResponseEntity<?> registroUsuario (@Valid @RequestBody RegistroRequest request) {
+        try {
+            AuthResponse response = authService.registrarUsuario(request);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
 }
