77f269485feff0860ac33d0dbc5fb497aafaf046
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-08-27 18:10:37 -0300

    inciarSesion  & cerrarSesion

--- cutly/cutly/src/main/java/com/dev/cutly/auth/controller/AuthController.java
+++ cutly/cutly/src/main/java/com/dev/cutly/auth/controller/AuthController.java
@@ -1,15 +1,13 @@
 package com.dev.cutly.auth.controller;
 
 import com.dev.cutly.auth.dto.AuthResponse;
+import com.dev.cutly.auth.dto.LoginRequest;
 import com.dev.cutly.auth.dto.RegistroRequest;
 import com.dev.cutly.auth.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -26,6 +24,27 @@ public class AuthController {
         try {
             AuthResponse response = authService.registrarUsuario(request);
             return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/iniciarSesion")
+    public ResponseEntity<?> inciarSesion (@Valid @RequestBody LoginRequest request) {
+        try {
+            AuthResponse response = authService.inciarSesion(request);
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/cerrarSesion")
+    public ResponseEntity<?> cerrarSesion (@RequestHeader("Authorization") String authHeader) {
+        try {
+            String token = authHeader.startsWith("Bearer ") ? authHeader.substring(7) : authHeader;
+            authService.cerrarSesion(token);
+            return ResponseEntity.noContent().build();
         } catch (RuntimeException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
--- cutly/cutly/src/main/java/com/dev/cutly/auth/service/AuthService.java
+++ cutly/cutly/src/main/java/com/dev/cutly/auth/service/AuthService.java
@@ -1,8 +1,11 @@
 package com.dev.cutly.auth.service;
 
 import com.dev.cutly.auth.dto.AuthResponse;
+import com.dev.cutly.auth.dto.LoginRequest;
 import com.dev.cutly.auth.dto.RegistroRequest;
+import com.dev.cutly.auth.model.TokenInvalido;
 import com.dev.cutly.auth.model.Usuario;
+import com.dev.cutly.auth.repository.TokenInvalidoRepository;
 import com.dev.cutly.auth.repository.UsuarioRepository;
 import com.dev.cutly.auth.security.JwtUtil;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,11 +17,13 @@ public class AuthService {
     private final UsuarioRepository usuarioRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
+    private final TokenInvalidoRepository tokenInvalidoRepository;
 
-    public AuthService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+    public AuthService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil, TokenInvalidoRepository tokenInvalidoRepository) {
         this.usuarioRepository = usuarioRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtUtil = jwtUtil;
+        this.tokenInvalidoRepository = tokenInvalidoRepository;
     }
 
     public AuthResponse registrarUsuario(RegistroRequest request) {
@@ -44,6 +49,26 @@ public class AuthService {
                 usuarioGuardado.getNombreUsuario(), usuarioGuardado.getNombre(),
                 usuarioGuardado.getApellido(), usuarioGuardado.getEmail()
         );
+    }
+
+    public AuthResponse inciarSesion (LoginRequest request) {
+        Usuario usuario = usuarioRepository.findByEmail(request.email())
+                .orElseThrow(() -> new RuntimeException("Credenciales invalidas: Usuario no existente"));
+
+        if (!passwordEncoder.matches(request.contrasena(), usuario.getContrasena())){
+            throw new RuntimeException("Credenciales invalidas: contraseña incorrecta");
+        }
+
+        String token = jwtUtil.generarToken(usuario.getEmail());
+
+        return new AuthResponse(token, usuario.getUsuarioId(), usuario.getNombreUsuario(),
+                usuario.getNombre(), usuario.getApellido(), usuario.getEmail());
+    }
+
+    public void cerrarSesion(String token) {
+        if(!tokenInvalidoRepository.existsByToken(token)) {
+            tokenInvalidoRepository.save(TokenInvalido.of(token));
+        }
     }
 
 }
